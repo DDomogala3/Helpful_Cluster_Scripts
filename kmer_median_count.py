11b57669eaadc54f18df6aaaeade53b5088f0e7e
DDomogala3 <ddd38@nau.edu>
2021-08-24 16:37:23 -0500

    Update kmer_median_count.py
    added script to count the number of median kmers of a certain size occurs across all reads. However, this need to be tested....

--- kmer_median_count.py
+++ kmer_median_count.py
@@ -1,9 +1,11 @@
+
 #!/usr/bin/env python3
 import os
+import mmh3
 import screed
 import itertools
 import argparse
-
+import statistics
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input",help = "the input fastq file")
@@ -15,7 +17,6 @@ k = args.k
 
 #sequence_file = "ERR6095742_pass.fastq"
 
-
 def kmer_count_better(k, sequence):
     '''
 #    Return a list of the number of times each possible k-mer appears
@@ -25,11 +26,17 @@ def kmer_count_better(k, sequence):
     rv = {}
     for read in screed.open(sequence):
         sequence = read.sequence
+    for i in range(0, len(sequence)-k+1):
+        read_subseq = sequence[i:1+k]
+        v = rv.get(read_subseq, 0)
         for i in range(0, len(sequence)-k+1):
-            subseq = sequence[i:i+k]
-            v = rv.get(subseq, 0)
-            rv[subseq] = v + 1
-
-    return list(rv.items()), len(sequence)
-print(kmer_count_better(k, sequence_file))
-             
+              subseq = sequence[i:i+k]
+              v = rv.get(subseq, 0)
+              rv[subseq] = v + 1
+              while v > 0:
+                  rv[read_subseq] = v + 1
+                  rv_len = len(rv)
+#                 print(rv)
+                  sumrv = sum(rv.values())
+                  sumrv_list = list(rv.values())
+                  print(statistics.median(sumrv_list))
