9b7b4520fdc681fcbcac1b2c904e2fee640a3ee7
DDomogala3 <ddd38@nau.edu>
2021-08-25 14:42:38 -0500

    Update kmer_median_count.py
    added updated code, still needs to be tested

--- kmer_median_count.py
+++ kmer_median_count.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 import os
 import screed
@@ -10,11 +9,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input",help = "the input fastq file")
 #args = parser.parse_args()
 parser.add_argument("k",help = "the kmer input size",type=int)
+#parser.add_argument("sn", help = "the number of sequences in the file", type = int)
 args = parser.parse_args()
 sequence_file = args.input
 k = args.k
-
+#sn = args.sn
 #sequence_file = "ERR6095742_pass.fastq"
+import sys
+import time
+#this is just a function I stole off of stackoverflow to produce a spinning cursor :)
+
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+spinner = spinning_cursor()
+for _ in range(50):
+    sys.stdout.write(next(spinner))
+    sys.stdout.flush()
+    time.sleep(0.1)
+    sys.stdout.write('\b')
 
 def kmer_count_better(k, sequence):
     '''
@@ -23,19 +38,17 @@ def kmer_count_better(k, sequence):
 #    '''
 
     rv = {}
-    for read in screed.open(sequence):
-        sequence = read.sequence
-    for i in range(0, len(sequence)-k+1):
-        read_subseq = sequence[i:1+k]
-        v = rv.get(read_subseq, 0)
+    for record in screed.open(sequence):
+        sequence = record.sequence
         for i in range(0, len(sequence)-k+1):
-              subseq = sequence[i:i+k]
-              v = rv.get(subseq, 0)
-              rv[subseq] = v + 1
-              while v > 0:
-                  rv[read_subseq] = v + 1
-                  rv_len = len(rv)
-#                 print(rv)
-                  sumrv = sum(rv.values())
-                  sumrv_list = list(rv.values())
-                  print(statistics.median(sumrv_list))
+               read_subseq = sequence[i:1+k]
+               v = rv.get(read_subseq, 0)
+               rv[read_subseq] = v + 1
+#              print(rv)
+#              print(sequence)
+               read_rvlist = list(rv.values())
+               statistics.median(read_rvlist)
+               break
+
+    return statistics.median(read_rvlist)
+print(kmer_count_better(k, sequence_file))
