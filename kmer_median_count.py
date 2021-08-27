107133092fb73e275e334b80acf1f3a106fb7f9f
DDomogala3 <ddd38@nau.edu>
2021-08-26 23:49:22 -0500

    Update kmer_median_count.py
    added simplified code for k-mer counting and file handling, added normalization as well

--- kmer_median_count.py
+++ kmer_median_count.py
@@ -4,21 +4,20 @@ import screed
 import itertools
 import argparse
 import statistics
-
+from collections import Counter
 parser = argparse.ArgumentParser()
 parser.add_argument("input",help = "the input fastq file")
 #args = parser.parse_args()
 parser.add_argument("k",help = "the kmer input size",type=int)
 #parser.add_argument("sn", help = "the number of sequences in the file", type = int)
 args = parser.parse_args()
-sequence_file = args.input
+input = args.input
 k = args.k
 #sn = args.sn
 #sequence_file = "ERR6095742_pass.fastq"
 import sys
 import time
-#this is just a function I stole off of stackoverflow to produce a spinning cursor :)
-
+#this is just a function I stole off of stackoverflow to produce a spinner
 def spinning_cursor():
     while True:
         for cursor in '|/-\\':
@@ -31,24 +30,52 @@ for _ in range(50):
     time.sleep(0.1)
     sys.stdout.write('\b')
 
-def kmer_count_better(k, sequence):
-    '''
-#    Return a list of the number of times each possible k-mer appears
-#    in seq, including overlapping occurrences.
-#    '''
+def readFastqFile(input):
+    """
+    Reads and returns file as FASTA format with special characters removed.
+    """
+    with screed.open(input) as seqfile:
+        for read in seqfile:
+            seq = read.sequence
+            for record in seqfile:
+                record = record.sequence
+    return seq
+seq = readFastqFile(input)
+record = readFastqFile(input)
 
-    rv = {}
-    for record in screed.open(sequence):
-        sequence = record.sequence
-        for i in range(0, len(sequence)-k+1):
-               read_subseq = sequence[i:1+k]
-               v = rv.get(read_subseq, 0)
-               rv[read_subseq] = v + 1
-#              print(rv)
-#              print(sequence)
-               read_rvlist = list(rv.values())
-               statistics.median(read_rvlist)
-               break
 
-    return statistics.median(read_rvlist)
-print(kmer_count_better(k, sequence_file))
+def kmer_count_better(sequence,ksize):
+    kmers = []
+    n_kmers = len(sequence) - ksize + 1
+    for i in range(n_kmers):
+        kmer = sequence[i:i + ksize]
+        kmers.append(kmer)
+    return kmers
+    print(kmers)
+
+
+km3 = kmer_count_better(seq, k)
+trimer = Counter(km3)
+
+
+
+
+def build_kmers(seq, ksize):
+    kmers = []
+    n_kmers = len(seq) - ksize + 1
+    db = screed.read_fastq_sequences(input)
+    for name in db:
+        full_seq = db[name].sequence
+        for i in range(n_kmers):
+            kmer = seq[i:i + ksize]
+            kmers.append(kmer)
+
+    return kmers
+
+km3 = build_kmers(seq, k)
+trimer = Counter(km3)
+total = statistics.median(trimer.values())
+print(total)
+for key in trimer:
+    trimer[key] /= total
+    print(trimer)
